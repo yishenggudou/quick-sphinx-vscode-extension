bab42b3498c4f4a9766a0b145c8c3be409948fa8
愧僧 <kuiseng.zhb@alibaba-inc.com>
2020-05-19 21:03:55 +0800

    init
    init

---
+++ src/extension.ts
@@ -0,0 +1,14 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand("extension.sayHello", () => {
+    vscode.window.showInformationMessage("Hello World!");
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+  /* */
+}
